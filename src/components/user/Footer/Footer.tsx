ded68b2cb3b7dbf792f33dc509decc3465787107
Lietreum <mirsieolien@gmail.com>
2024-08-30 06:38:33 +0700

    footr chng

--- src/components/user/Footer/Footer.tsx
+++ src/components/user/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <footer className="bg-transparent text-center py-4">
-            <p className="text-gray-600">Developed By Zaky!!!!! panas tiris!!!!!!!!!</p>
+            <p className="text-gray-600">Developed By Zaky!!!!! bejaan ijin!</p>
         </footer>
     );
 };
