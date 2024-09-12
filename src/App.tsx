98bde6d56d9789d7d29eb756709f135624b34e7b
Lietreum <mirsieolien@gmail.com>
2024-09-12 23:59:41 +0700

    // @ts-nocheck

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-// @ts-ignore
+// @ts-nocheck
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation, } from "react-router-dom";
-//dunno when i delete the Router it will go nuts, dont delete it or maybe find some solution 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+//dunno when i delete the Router it will go nuts, dont delete it or maybe find some solution
 // mf how the live demo suppose works
 import Loader from "./common/Loader";
 import PageTitle from "./components/admin/PageTitle";
