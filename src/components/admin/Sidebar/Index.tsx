23f55ad848207b72c1c5fad416e1aac24431b10a
Lietreum <mirsieolien@gmail.com>
2024-10-04 13:34:40 +0700

    kasir done

--- src/components/admin/Sidebar/Index.tsx
+++ src/components/admin/Sidebar/Index.tsx
@@ -68,7 +68,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     >
       {/* Sidebar Header */}
       <div className="flex items-center justify-center px-10 py-0 bg-[#0F1E34]">
-        <NavLink to="/">
+        <NavLink to="#">
           <img src={Logo} alt="Logo" className="w-50 h-auto" />
         </NavLink>
 
