7f0a208577ea84a84fb46b4b2b04314088c7b62e
fGHG99 <azkalearn10@gmail.com>
2024-10-18 07:40:48 +0700

    fetching store

--- src/pages/siswa/Homepage.tsx
+++ src/pages/siswa/Homepage.tsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { Container, Grid, Box } from "@mui/material";
 import CategoryCard from "../../components/user/CategoryCard/CategoryCard"; 
-import BannerData from "../../Helpers/HomePageBanner";
 import AnimatedText from "../../components/AnimatedText";
 import WalletCard from "../../components/user/Header/WalletCard";
-// import SearchBar from "../../components/user/SearchBar/SearchBar";
 import Profileholder from "../../assets/admin/images/user/user-01.png"
-// import Footer from "../../components/user/Footer/Footer";
+
+enum TokoTypes {
+  Kantin = 'Kantin',
+  Hydro = 'Hydro',
+  Koperasi = 'Koperasi',
+}
+
+interface Store {
+  tokoId: string;
+  name: string;
+  img: string;
+  desc: string;
+  tokoType: TokoTypes;
+}
 
 const Homepage: React.FC = () => {
+  const [stores, setStores] = useState<Store[]>([]);
+
+  useEffect(() => {
+    // Fetch data from the backend API
+    const fetchStores = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/store/');
+        const data = await response.json();
+        setStores(data); // Update the state with the fetched data
+      } catch (error) {
+        console.error('Error fetching store data:', error);
+      }
+    };
+
+    fetchStores();
+  }, []); // Empty dependency array means this will run once when the component mounts
+
   return (
     <Container
       maxWidth="xl"
@@ -72,20 +100,20 @@ const Homepage: React.FC = () => {
 
       {/* Store Cards Section */}
       <Grid
-        container
-        spacing={4}
-        justifyContent="center"
-        sx={{
-          maxWidth: "1200px",
-          margin: "0 auto",
-        }}
-      >
-        {BannerData.map((data) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={data.img}>
-            <CategoryCard data={data} />
-          </Grid>
-        ))}
-      </Grid>
+      container
+      spacing={4}
+      justifyContent="center"
+      sx={{
+        maxWidth: "1200px",
+        margin: "0 auto",
+      }}
+    >
+      {stores.map((store) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={store.tokoId}>
+          <CategoryCard data={store} /> 
+        </Grid>
+      ))}
+    </Grid>
     </Container>
   );
 };
